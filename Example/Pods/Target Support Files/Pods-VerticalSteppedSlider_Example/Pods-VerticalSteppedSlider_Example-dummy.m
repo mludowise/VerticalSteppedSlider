16671e2ebc76dd83661fc0300d4317cae06e0bd2
mludowise <mludowise@gmail.com>
2017-09-27 12:19:49 -0700

    pod lib create

---
+++ Example/Pods/Target Support Files/Pods-VerticalSteppedSlider_Example/Pods-VerticalSteppedSlider_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VerticalSteppedSlider_Example : NSObject
+@end
+@implementation PodsDummy_Pods_VerticalSteppedSlider_Example
+@end
---
+++ Example/Pods/Target Support Files/VerticalSteppedSlider/VerticalSteppedSlider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VerticalSteppedSlider : NSObject
+@end
+@implementation PodsDummy_VerticalSteppedSlider
+@end
