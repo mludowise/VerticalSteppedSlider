c9b36bb19bbe3c53165651777045683b6906f7d2
mludowise <mludowise@gmail.com>
2017-10-16 16:40:36 -0700

    Updated tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -5,10 +5,10 @@ import Nimble
 import Nimble_Snapshots
 import VerticalSteppedSlider
 
-class TableOfContentsSpec: QuickSpec {
+class TestVSSlider: QuickSpec {
     
     override func spec() {
-        describe("appearance vertical") {
+        describe("slider") {
             let frame = CGRect(x: 0, y: 0, width: 400, height: 400)
             var slider: VSSlider!
 
@@ -21,98 +21,94 @@ class TableOfContentsSpec: QuickSpec {
             }
             
             it("width") {
+                slider.trackWidth = 10
+                expect(slider) == snapshot()
+            }
+            
+            it("colors") {
+                slider.maximumTrackTintColor = .green
+                slider.minimumTrackTintColor = .red
+                expect(slider) == snapshot()
+            }
+            
+            it("track img") {
+                slider.maximumTrackImage = #imageLiteral(resourceName: "max-track")
+                slider.minimumTrackImage = #imageLiteral(resourceName: "min-track")
+                slider.trackWidth = 10
+                expect(slider) == snapshot()
+            }
+            
+            it("not extended under thumb") {
+                slider.trackExtendsUnderThumb = false
+                expect(slider) == snapshot()
+            }
+            
+            it("ascending") {
+                slider.ascending = true
+                expect(slider) == snapshot()
+            }
+            
+            it("thumb img") {
+                slider.thumbImage = #imageLiteral(resourceName: "thumb")
+                expect(slider) == snapshot()
+            }
+            
+            it("thumb color") {
+                slider.thumbTintColor = .purple
+                expect(slider) == snapshot()
+            }
+            
+            it("colors and width") {
                 slider.maximumTrackTintColor = .green
                 slider.minimumTrackTintColor = .red
                 slider.trackWidth = 10
-                expect(slider) == recordSnapshot()
+                expect(slider) == snapshot()
             }
-            
-//            it("colors") {
-//                slider.maximumTrackTintColor = .green
-//                slider.minimumTrackTintColor = .red
-////                slider.markColor = .yellow
-////                slider.markWidth = 3
-//                expect(slider) == recordSnapshot()
-//            }
             
             it("marks") {
-                
+                slider.increment = 0.1
+                slider.markColor = .yellow
+                slider.markWidth = 3
+                expect(slider) == snapshot()
             }
-        }
-        
-        describe("appearance horizontal") {
-            let frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-            var slider: VSSlider!
             
-            beforeEach {
-                slider = VSSlider(frame: frame)
-                slider.minimumValue = -1
-                slider.maximumValue = 1
-                slider.increment = 0.25
-                slider.value = 0
+            it("no marks") {
+                slider.increment = 0
+                slider.markColor = .yellow
+                slider.markWidth = 3
+                expect(slider) == snapshot()
+            }
+            
+            it("horizontal") {
                 slider.vertical = false
-            }
-            
-            it("width") {
                 slider.maximumTrackTintColor = .green
                 slider.minimumTrackTintColor = .red
                 slider.trackWidth = 10
-                expect(slider) == recordSnapshot()
+                expect(slider) == snapshot()
             }
             
-            //            it("colors") {
-            //                slider.maximumTrackTintColor = .green
-            //                slider.minimumTrackTintColor = .red
-            ////                slider.markColor = .yellow
-            ////                slider.markWidth = 3
-            //                expect(slider) == recordSnapshot()
-            //            }
+            it("set value") {
+                slider.value = 0.25
+                expect(slider.value).toEventually(equal(0.25))
+            }
             
-            it("marks") {
-                
+            it("round value") {
+                slider.value = 0.4
+                expect(slider.roundedValue).toEventually(equal(0.5))
+            }
+            
+            it("update increment") {
+                slider.value = 0.75
+                slider.increment = 1
+                expect(slider.roundedValue).toEventually(equal(1))
+            }
+            
+            it("no rounding") {
+                slider.increment = 0
+                slider.value = 0.4
+                expect(slider.roundedValue).toEventually(equal(0.4))
             }
         }
-        
-//        describe("these will fail") {
-//
-//            it("can do maths") {
-//                expect(1) == 2
-//            }
-//
-//            it("can read") {
-//                expect("number") == "string"
-//            }
-//
-//            it("will eventually fail") {
-//                expect("time").toEventually( equal("done") )
-//            }
-//
-//            context("these will pass") {
-//
-//                it("can do maths") {
-//                    expect(23) == 23
-//                }
-//
-//                it("can read") {
-//                    expect("🐮") == "🐮"
-//                }
-//
-//                it("will eventually pass") {
-//                    var time = "passing"
-//
-//                    DispatchQueue.main.async {
-//                        time = "done"
-//                    }
-//
-//                    waitUntil { done in
-//                        Thread.sleep(forTimeInterval: 0.5)
-//                        expect(time) == "done"
-//
-//                        done()
-//                    }
-//                }
-//            }
-//        }
     }
     
 }
